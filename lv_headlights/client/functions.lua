69430b8390e78e42a772674f00beae67c420ccd5
DonSamuele <66538187+DonSamuelee@users.noreply.github.com>
2022-09-05 21:00:21 +0200

    Better and optimized code

--- lv_headlights/client/functions.lua
+++ lv_headlights/client/functions.lua
@@ -54,65 +54,33 @@ function ColourList()
             elements = elems
         },
         function(data, menu)
+            local color = tonumber(data.current.value)
+            local veh = GetVehiclePedIsUsing(PlayerPedId())
+            ToggleVehicleMod(veh, 22, true) -- Xenon
             if data.current.value == '1' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SetVehicleXenonLightsColour(veh, 1)
-            end
-            if data.current.value == '2' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SetVehicleXenonLightsColour(veh, 2)
-            end
-            if data.current.value == '3' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SetVehicleXenonLightsColour(veh, 3)
-            end
-            if data.current.value == '4' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SetVehicleXenonLightsColour(veh, 4)
-            end
-            if data.current.value == '5' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SetVehicleXenonLightsColour(veh, 5)
-            end
-            if data.current.value == '6' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SetVehicleXenonLightsColour(veh, 6)
-            end
-            if data.current.value == '7' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SetVehicleXenonLightsColour(veh, 7)
-            end
-            if data.current.value == '8' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SetVehicleXenonLightsColour(veh, 8)
-            end
-            if data.current.value == '9' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SetVehicleXenonLightsColour(veh, 9)
-            end
-            if data.current.value == '10' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SSetVehicleXenonLightsColour(veh, 10)
-            end
-            if data.current.value == '11' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SSetVehicleXenonLightsColour(veh, 11)
-            end
-            if data.current.value == '12' then
-            local veh = GetVehiclePedIsUsing(PlayerPedId())
-            ToggleVehicleMod(veh, 22, true) -- Xenon
-            SetVehicleXenonLightsColour(veh, 12)
+                SetVehicleXenonLightsColour(veh, 1)
+            elseif data.current.value == '2' then
+                SetVehicleXenonLightsColour(veh, color)
+            elseif data.current.value == '3' then
+                SetVehicleXenonLightsColour(veh, color)
+            elseif data.current.value == '4' then
+                SetVehicleXenonLightsColour(veh, color)
+            elseif data.current.value == '5' then
+                SetVehicleXenonLightsColour(veh, color)
+            elseif data.current.value == '6' then
+                SetVehicleXenonLightsColour(veh, color)
+            elseif data.current.value == '7' then
+                SetVehicleXenonLightsColour(veh, color)
+            elseif data.current.value == '8' then
+                SetVehicleXenonLightsColour(veh, color)
+            elseif data.current.value == '9' then
+                SetVehicleXenonLightsColour(veh, color)
+            elseif data.current.value == '10' then
+                SSetVehicleXenonLightsColour(veh, color)
+            elseif data.current.value == '11' then
+                SSetVehicleXenonLightsColour(veh, color)
+            elseif data.current.value == '12' then
+                SetVehicleXenonLightsColour(veh, color)
             end
     end,
     function(data, menu)
@@ -132,48 +100,43 @@ local location = {
 
 }
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
-        Citizen.Wait(0)
-        for k in pairs(location) do
-            DrawMarker(20, location[k].x, location[k].y, location[k].z, 0, 0, 0, 0, 0, 0, 0.8, 0.8, 0.8, 255, 50, 255, 100, true, true, 2, true, false, false, false)
-        end
-    end
-end)
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
+        idle = 800
 
         for k in pairs(location) do
 
             local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
             local dist = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, location[k].x, location[k].y, location[k].z)
-
+            if dist <= 10.0 then
+                idle = 0
+                DrawMarker(20, location[k].x, location[k].y, location[k].z, 0, 0, 0, 0, 0, 0, 0.8, 0.8, 0.8, 255, 50, 255, 100, true, true, 2, true, false, false, false)
+            end
             if dist <= 1.2 then
+                idle = 0
                 ESX.ShowHelpNotification("Press ~INPUT_TALK~ to change the ~p~Colour~s~")
-				if IsControlJustPressed(1,51) then 
-					OpenHeadlightMenu()
-				end
+		if IsControlJustPressed(1,51) then 
+			OpenHeadlightMenu()
+		end
             end
         end
+        Wait(idle)
     end
 end)
 
+-- Create blips
+CreateThread(function()
+    for k in pairs(location) do
+        local blip = AddBlipForCoord(-227.39, -2659.0, 6.0)
 
-	-- Create blips
-	Citizen.CreateThread(function()
-        for k in pairs(location) do
-			local blip = AddBlipForCoord(-227.39, -2659.0, 6.0)
+        SetBlipSprite (blip, 643)
+        SetBlipDisplay(blip, 4)
+        SetBlipScale  (blip, 1.0)
+        SetBlipColour (blip, 8)
+        SetBlipAsShortRange(blip, true)
 
-			SetBlipSprite (blip, 643)
-			SetBlipDisplay(blip, 4)
-			SetBlipScale  (blip, 1.0)
-			SetBlipColour (blip, 8)
-			SetBlipAsShortRange(blip, true)
-
-			BeginTextCommandSetBlipName("STRING")
-			AddTextComponentString("Headlight Changer")
-			EndTextCommandSetBlipName(blip)
-		end
-	end)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString("Headlight Changer")
+        EndTextCommandSetBlipName(blip)
+    end
+end)
